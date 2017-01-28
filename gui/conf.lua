3ea22c1c3b2aaa88434a6be6283071fc8055798f
calebjones422 <caleb@anthonyandtobie.com>
2017-01-27 21:05:59 -0600

    Haven't commited in a while so doing it

--- gui/conf.lua
+++ gui/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
 	t.title = '' -- The title of the window the game is in (string)
-	t.version = '0.10.1'         -- The LÖVE version this game was made for (string)
+	t.version = '0.10.2'         -- The LÖVE version this game was made for (string)
 	t.window.width = 430
 	t.window.height = 430
 end
--- gui/main.lua
+++ gui/main.lua
@@ -1,5 +1,6 @@
 debug = true
 screen = 1
+back = true
 function love.load(arg)
    logo = love.graphics.newImage('Assets/Logo.png')
    butoon1 = love.graphics.newImage('Assets/Setupbutoon.png')
@@ -21,14 +22,28 @@ down = love.mouse.isDown(1)
   -- Tabs
     if x >= 10 and x <= 145 and y >= 122 and y <= 155 then
       screen = 1
+      back = true
     elseif x >= 145 and x <= 285 and y >= 120 and y <= 155 then
-      screen = 2
+      back= true
     end
 
     if screen == 1 then
       -- Tab1 / Setup Buttons
       if x >= 20 and x <= 60 and y >= 170 and y <= 210 then
       screen = 3
+      back = false
+      elseif x >= 20 and x <= 70 and y >= 220 and y <= 260 then
+      screen = 4
+      back = false
+      elseif x >= 20 and x <= 70 and y >= 270 and y <= 310 then
+      screen = 5
+      back = false
+      elseif x >= 20 and x <= 75 and y >= 320 and y <= 360 then
+      screen = 6
+      back = false
+      elseif x >= 20 and x <= 100 and y >= 370 and y <= 410 then
+      screen = 7
+      back = false
       end
     elseif screen == 2 then
       -- Tab2 / Option Buttons
@@ -38,11 +53,13 @@ end
 
 function love.draw(dt)
   love.graphics.draw(background, 0, 0)
-  love.graphics.draw(logo, 50, 20)
-  love.graphics.draw(butoon1, 10, 120)
-  love.graphics.draw(butoon2, 145, 118)
-  love.graphics.print("X:"..x, 0, 0)
-  love.graphics.print("Y:"..y, 0, 10)
+  if back == true then
+    love.graphics.draw(logo, 50, 20)
+    love.graphics.draw(butoon1, 10, 120)
+    love.graphics.draw(butoon2, 145, 118)
+    love.graphics.print("X:"..x, 0, 0)
+    love.graphics.print("Y:"..y, 0, 10)
+  end
   if screen == 1 then
     love.graphics.draw(Tbutoon1, 20, 170)
     love.graphics.draw(Tbutoon2, 20, 220)
@@ -52,8 +69,16 @@ function love.draw(dt)
   elseif screen == 2 then
 
   elseif screen == 3 then
-  love.graphics.print("SCREEN3",0,20)
+    love.graphics.print("Lua",0,20)
+  elseif screen == 4 then
+    love.graphics.print("Love",0,20)
+  elseif screen == 5 then
+    love.graphics.print("Java",0,20)
+  elseif screen == 6 then
+    love.graphics.print("HTML",0,20)
+  elseif screen == 7 then
+    love.graphics.print("Shellscript",0,20)
   else
-  screen = 1
+    screen = 1
   end
 end
